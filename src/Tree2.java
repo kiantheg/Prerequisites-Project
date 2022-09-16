43e1b10de67a1657ee9f0777a6baddfe14c15bb3
wyatt <wanand1@hwemail.com>
2022-09-16 12:53:02 -0700

    Copy pasted the tree into project because it was not visible on eclipse

---
+++ src/Tree2.java
@@ -0,0 +1,66 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
+public class Tree2 {
+
+	public Tree2(ArrayList<String> arr) throws IOException {
+		String sha1 = encryptThisString(generateString(arr));
+		
+		File file = new File(sha1);
+		FileWriter fw = new FileWriter(file);
+		BufferedWriter bw = new BufferedWriter(fw);
+		for (String str: arr) {
+			bw.write(str);
+			bw.newLine();
+		}
+		fw.close();
+		bw.close();
+	}
+	
+	private String generateString(ArrayList<String> arr) {
+		String string = "";
+		for (String str: arr) {
+			string+=str;
+		}
+		return string;
+	}
+	
+	//SHA1 method
+		private String encryptThisString(String input)
+	    {
+	        try {
+	            // getInstance() method is called with algorithm SHA-1
+	            MessageDigest md = MessageDigest.getInstance("SHA-1");
+	 
+	            // digest() method is called
+	            // to calculate message digest of the input string
+	            // returned as array of byte
+	            byte[] messageDigest = md.digest(input.getBytes());
+	 
+	            // Convert byte array into signum representation
+	            BigInteger no = new BigInteger(1, messageDigest);
+	 
+	            // Convert message digest into hex value
+	            String hashtext = no.toString(16);
+	 
+	            // Add preceding 0s to make it 32 bit
+	            while (hashtext.length() < 32) {
+	                hashtext = "0" + hashtext;
+	            }
+	 
+	            // return the HashText
+	            return hashtext;
+	        }
+	 
+	        // For specifying wrong message digest algorithms
+	        catch (NoSuchAlgorithmException e) {
+	            throw new RuntimeException(e);
+	        }
+	    }
+}
